9766b84e0c6ffab96095e2b88e1407067d7786e5
Irene Casares Rodríguez <i62caroi@uco.es>
2023-11-29 13:45:13 +0100

    Guardar códigos decodificados sin repeticion Al escanear códigos, guarda los leidos en un fichero decode_objects.txt sin repetirlos.

--- examples/ESP32 (testeando)/STREAMING SERVER AND PYTHON ACCESS/barcode_scanner_from_server.py
+++ examples/ESP32 (testeando)/STREAMING SERVER AND PYTHON ACCESS/barcode_scanner_from_server.py
@@ -3,6 +3,7 @@ import cv2
 import pyzbar.pyzbar as pyzbar
 import numpy as np
 
+# Decodificar código de barras en imagen (im)
 def decode(im):
     # Decodifica los códigos de barras de la imagen
     decodedObjects = pyzbar.decode(im)
@@ -39,12 +40,26 @@ def display(im, decodedObjects):
     cv2.imshow("Video Stream", im)
 
 
+# Guardar código decodificado en un fichero sin repetirlo
+def saveDecodeObject(decodedObjects):
+    with open('decoded_objects.txt', 'a') as f:
+        for obj in decodedObjects:
+            # Si no se ha guardado ya ese objeto, se guarda en fichero y en el conjunto
+            if obj.data not in saved_objects: 
+                print("\n\n--------- Guardando objeto -------------\n\n")
+                f.write('Type: ' + str(obj.type) + '\n' + 'Data: ' + str(obj.data) + '\n\n')
+                #f.write(str(obj.data) + '\n')
+                saved_objects.add(obj.data)
+
+
+# Conjunto vacío (los conjuntos no permiten duplicados)
+saved_objects = set() # Global
 
 # Main
 if __name__ == '__main__':
     
     # Abre el stream de vídeo del ESP32-CAM
-    url = 'http://192.168.222.42:81/stream'
+    url = 'http://192.168.222.114:81/stream'
     cap = cv2.VideoCapture(url)
 
     #cap = cv2.VideoCapture(0)
@@ -61,6 +76,9 @@ if __name__ == '__main__':
 
         # Decodificar código de barras (si hay)
         decodedObjects = decode(frame)
+        
+        # Guardar código si no se ha guardado ya
+        saveDecodeObject(decodedObjects)
 
         cv2.imshow("Video Stream", frame)
         
