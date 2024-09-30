3d99e7de916ae7052004e14dc2808e8d73e476af
Irene Casares Rodríguez <i62caroi@uco.es>
2024-10-01 00:41:37 +0200

    Create updateValoresNutricionales.sql

---
+++ Docs/Diseño-SmartCloth/Database/updateValoresNutricionales.sql
@@ -0,0 +1,31 @@
+-- Actualizar los valores nutricionales de los grupos del 1 al 20
+UPDATE tipo_alimentos SET kcal_g = 0.7105, prot_g = 0.0378, lip_g = 0.0414, carb_g = 0.0515 WHERE id = 1;
+UPDATE tipo_alimentos SET kcal_g = 0.4729, prot_g = 0.0332, lip_g = 0.0174, carb_g = 0.0495 WHERE id = 2;
+UPDATE tipo_alimentos SET kcal_g = 0.3393, prot_g = 0.0338, lip_g = 0.0028, carb_g = 0.0478 WHERE id = 3;
+UPDATE tipo_alimentos SET kcal_g = 0.8598, prot_g = 0.0301, lip_g = 0.0257, carb_g = 0.1316 WHERE id = 4;
+UPDATE tipo_alimentos SET kcal_g = 1.8484, prot_g = 0.0356, lip_g = 0.0806, carb_g = 0.2478 WHERE id = 5;
+UPDATE tipo_alimentos SET kcal_g = 0.4809, prot_g = 0.0082, lip_g = 0.0025, carb_g = 0.1119 WHERE id = 6;
+UPDATE tipo_alimentos SET kcal_g = 0.2454, prot_g = 0.0148, lip_g = 0.0037, carb_g = 0.0412 WHERE id = 7;
+UPDATE tipo_alimentos SET kcal_g = 2.1053, prot_g = 0.0622, lip_g = 0.0142, carb_g = 0.4452 WHERE id = 8;
+UPDATE tipo_alimentos SET kcal_g = 3.2236, prot_g = 0.2148, lip_g = 0.0327, carb_g = 0.5523 WHERE id = 9;
+UPDATE tipo_alimentos SET kcal_g = 3.8755, prot_g = 0.0707, lip_g = 0.1883, carb_g = 0.5055 WHERE id = 10;
+UPDATE tipo_alimentos SET kcal_g = 7.2434, prot_g = 0.0283, lip_g = 0.7874, carb_g = 0.0275 WHERE id = 11;
+UPDATE tipo_alimentos SET kcal_g = 8.3561, prot_g = 0.0232, lip_g = 0.9312, carb_g = 0.0066 WHERE id = 12;
+UPDATE tipo_alimentos SET kcal_g = 5.6178, prot_g = 0.0126, lip_g = 0.6115, carb_g = 0.0178 WHERE id = 13;
+UPDATE tipo_alimentos SET kcal_g = 8.2686, prot_g = 0.0107, lip_g = 0.9136, carb_g = 0.0007 WHERE id = 14;
+UPDATE tipo_alimentos SET kcal_g = 3.5162, prot_g = 0.0262, lip_g = 0.0247, carb_g = 0.8446 WHERE id = 15;
+UPDATE tipo_alimentos SET kcal_g = 0.9947, prot_g = 0.1930, lip_g = 0.0176, carb_g = 0.0101 WHERE id = 16;
+UPDATE tipo_alimentos SET kcal_g = 1.3225, prot_g = 0.1814, lip_g = 0.0505, carb_g = 0.0375 WHERE id = 17;
+UPDATE tipo_alimentos SET kcal_g = 1.5525, prot_g = 0.1347, lip_g = 0.1126, carb_g = 0.0064 WHERE id = 18;
+UPDATE tipo_alimentos SET kcal_g = 2.7904, prot_g = 0.2061, lip_g = 0.2160, carb_g = 0.0000 WHERE id = 19;
+UPDATE tipo_alimentos SET kcal_g = 3.1614, prot_g = 0.1316, lip_g = 0.2817, carb_g = 0.0364 WHERE id = 20;
+
+
+-- Actualizar los valores nutricionales de los grupos 21 a 27 (cocinados)
+UPDATE tipo_alimentos SET kcal_g = 0.2282, prot_g = 0.0158, lip_g = 0.0098, carb_g = 0.0235 WHERE id = 21;
+UPDATE tipo_alimentos SET kcal_g = 0.9617, prot_g = 0.0195, lip_g = 0.0051, carb_g = 0.2375 WHERE id = 22;
+UPDATE tipo_alimentos SET kcal_g = 1.1762, prot_g = 0.0858, lip_g = 0.0146, carb_g = 0.1868 WHERE id = 23;
+UPDATE tipo_alimentos SET kcal_g = 0.9450, prot_g = 0.1978, lip_g = 0.0131, carb_g = 0.0013 WHERE id = 24;
+UPDATE tipo_alimentos SET kcal_g = 1.6939, prot_g = 0.2269, lip_g = 0.0728, carb_g = 0.0258 WHERE id = 25;
+UPDATE tipo_alimentos SET kcal_g = 1.4769, prot_g = 0.1298, lip_g = 0.1067, carb_g = 0.0064 WHERE id = 26;
+UPDATE tipo_alimentos SET kcal_g = 2.8156, prot_g = 0.1869, lip_g = 0.2260, carb_g = 0.0000 WHERE id = 27;
